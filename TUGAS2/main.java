3c7acbf49162a148fbfd9af9cdf539b4c619428b
salmaa00 <132159672+salmaa00@users.noreply.github.com>
2023-07-07 00:31:58 +0700

    Update main.java

--- TUGAS2/main.java
+++ TUGAS2/main.java
@@ -4,24 +4,24 @@ package TUGAS2;
 public class main {
 
     public static void main(String[] args) {
-         karyawan manusia1 = new karyawan ();
-        manusia1.NIP      = "227064516069";
-        manusia1.nama     = "Salma Salsabila";
-        manusia1.kelamin  = "Perempuan";
-        manusia1.jmlhari  = 28;
-        manusia1.karyawan ();
-        manusia1.AbsenKerja();
-        manusia1.HitungGaji();
-        System.out.println();
+        karyawan manusia1 = new karyawan (); //memanggil class karyawan dan memasukkan data manusia 1
+        manusia1.NIP      = "227064516069"; //memasukkan data NIP manusia 1
+        manusia1.nama     = "Salma Salsabila"; //memasukkan data nama manusia 1
+        manusia1.kelamin  = "Perempuan"; //memasukkan data kelamin manusia 1
+        manusia1.jmlhari  = 28; //memasukkan data jumlah hari manusia 1
+        manusia1.karyawan (); //memanggil class karyawan
+        manusia1.AbsenKerja(); //memanggil class Absen Kerja
+        manusia1.HitungGaji(); //memanggil class Hitung Gaji
+        System.out.println(); 
         
-        karyawan manusia2 = new karyawan ();
-        manusia2.NIP      = "227064516100";
-        manusia2.nama     = "Muhammad Raysa Farhan";
-        manusia2.kelamin  = "Laki-Laki";
-        manusia2.jmlhari  = 25;
-        manusia2.karyawan();
-        manusia2.AbsenKerja();
-        manusia2.HitungGaji();
+        karyawan manusia2 = new karyawan (); //memanggil class karyawan dan memasukkan data manusia 2
+        manusia2.NIP      = "227064516100"; //memasukkan data NIP manusia 2
+        manusia2.nama     = "Muhammad Raysa Farhan"; //memasukkan data nama manusia 2
+        manusia2.kelamin  = "Laki-Laki"; //memasukkan data kelamin manusia 2
+        manusia2.jmlhari  = 25; //memasukkan data jumlah hari manusia 2
+        manusia2.karyawan(); //memanggil class karyawan
+        manusia2.AbsenKerja(); //memanggil class Absen Kerja
+        manusia2.HitungGaji(); //memanggil class Hitung Gaji
     }
 }
    
